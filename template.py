619d9dc502245b9fa016dc6479d52f0ec880aa71
Michelle Nguyen <michellehnguyen03@gmail.com>
2023-03-23 12:06:14 -0500

    reduced errors, fixed syntax, clarified meaning of this library file

--- template.py
+++ template.py
@@ -1,6 +1,17 @@
 import uuid
 import bcrypt
 
+
+
+# This file is just a library of SQL functions; no connection is actually being done here.
+# The "cur" input variable comes from:
+# con = psycopg2.connect("host="",
+#     database="",
+#     user="",
+#     password="")
+# cur = con.cursor()
+# print("Connected to Postgres")
+
 # need to create user class
 
 
@@ -22,38 +33,42 @@ def hash_pw(password):
     return result
 
 
-@app.route('/registration', methods=['POST'])
-def insert_user():
+# @app.route('/registration', methods=['POST']) -> use this when connecting db and routing html
+def insert_user(cur, user_fname,user_lname, user_addr,p_number,user_sex, user_dob,membership):
     # generate uuid
     user_uuid = str(uuid.uuid4())
-    user_fname = request.form['user_fname']
-    user_lname = request.form['user_lname']
-    user_email = request.form['user_email']
-    user_password = request.form['user_password']
+    # do the request in dif. file
+        # user_fname = request.form['user_fname']
+        # user_lname = request.form['user_lname']
+        # user_email = request.form['user_email']
+        # user_password = request.form['user_password']
 
    # insert user into database
-    try:
-        result = cur.execute("""INSERT INTO user_account (user_id, first_name, last_name, email, password)
-                               VALUES (?, ?, ?, ?, ?)""", (user_uuid, user_fname, user_lname, user_email, user_password))
-        db.commit()
-        return 'User registered successfully!'
-    except:
-        db.rollback()
-        return 'User registration failed.'
+    cur.execute("""INSERT INTO user_account VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""", (user_uuid, user_fname,user_lname, user_addr, p_number, user_sex, user_dob,membership))
+  
 
 
 def insert_user_login(cur, user_role, user_name, pw, login_at ):
     # email is used as username
     user_uuid = cur.execute("""SELECT * FROM user_account WHERE email=?""", user_name)
     hashed = hash_pw(pw)
-    result = cur.execute("""INSERT INTO user_login (user_id, user_role, user_name, hashed_password, login_at)
-                            VALUES (?,?,?,?,?)""", (user_uuid,user_role, user_name, hashed, login_at))
+    cur.execute("""INSERT INTO user_login VALUES (%s, %s, %s, %s, %s)""", (user_uuid,user_role, user_name, hashed, login_at))
+    
+    # note: this part is completely separate from this function; we will use it when connecting to db!
+    # try:
+        # call the insert_user_login function!
+    #     insert_user_login(cur, user_name, user_password, login_at)
+    #     cur.commit()
+    #     return 'User registered successfully!'
+    # except:
+    #     cur.rollback()
+    #     return 'User registration failed.'
 
 
 # WARNING: the area below is all pseudo or unfinished code                     
 
 def insert_art(cur, obj_num,title, artist, culture, made_on, obj_type, art_dpt, dim):
-    sql_query = "INSERT INTO artworks (object_number, artwork_title, artist, culture, made_on, object_type, art_department, dimensions) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    sql_query = """INSERT INTO artworks VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
     values = (obj_num, title, artist, culture, made_on, obj_type, art_dpt, dim)
     try:
         cur.execute(sql_query, values)
@@ -65,34 +80,42 @@ def insert_art(cur, obj_num,title, artist, culture, made_on, obj_type, art_dpt, 
 
 def insert_gift_item(cur, gift_sku, gift_name, gift_type, gift_price):
     try:
-        sql_query = "INSERT INTO gift_shop_item (gift_SKU, gift_name, gift_type, gift_price) VALUES (%s, %s, %s, %s);"
+        sql_query = "INSERT INTO gift_shop_item VALUES (%s, %s, %s, %s);"
         cur.execute(sql_query, (gift_sku, gift_name, gift_type, gift_price))
     except Exception as e:
         return f"Error inserting gift item: {e}"
     else:
+        cur.commit()
         return "Gift item inserted successfully."
+    
 
 
 def insert_gift_sales(cur, transac_id, gift_sku, transac_at, user_id):
     try:
-        # Extract values from the request body
-        gift_sku = request.json['gift_sku']
-        transac_at = request.json['transaction_at']
-        user_id = request.json['user_id']
+        # Extract values from the request body -> do this in dif. file
+        # gift_sku = request.json['gift_sku']
+        # transac_at = request.json['transaction_at']
+        # user_id = request.json['user_id']
 
         # Generate a unique transaction ID using the uuid4() function
-        transac_id = uuid4()
+        transac_id = str(uuid.uuid4())
 
         # Insert the values into the gift_shop_sales table
-        sql = "INSERT INTO gift_shop_sales (gift_transaction_id, gift_SKU, gift_transaction_at, user_id) VALUES (%s, %s, %s, %s)"
-        cur.execute(sql, (transac_id, gift_sku, transac_at, user_id))
-        conn.commit()
+        sql = "INSERT INTO gift_shop_sales VALUES (%s, %s, %s, %s)"
+        try:
+            cur.execute(sql, (transac_id, gift_sku, transac_at, user_id))
+      
+        except Exception as e:
+            print(f"Error inserting values into gift table: {e}")
+        else:
+            print("Values inserted successfully!")
+        cur.commit()
 
         # Return a success message
         return jsonify({'message': 'Gift sale inserted successfully.'}), 201
     except Exception as e:
         # If any error occurs, rollback the transaction and return an error message
-        conn.rollback()
+        cur.rollback()
         return jsonify({'error': str(e)}), 400
     finally:
         # Close the database connection
