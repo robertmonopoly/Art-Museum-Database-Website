87fb70332a94338caa81fdda3eba4ede7aa2b5be
Michelle Nguyen <michellehnguyen03@gmail.com>
2023-03-25 14:48:40 -0500

    updating more insertions

--- script.sql
+++ script.sql
@@ -23,7 +23,9 @@ INSERT INTO gift_shop_item VALUES ('pai-ang-bla', 'paintbrush','supplies','9.99'
 
 INSERT INTO gift_shop_sales VALUES ('3457ab7b-9ecb-48e2-a8d0-2b921fe12747', 'toteb-art2', '2023-03-27 10:36:01','e3a9dd72-cad1-11ed-afa1-0242ac120002');
 
-INSERT INTO gift_shop_sales VALUES ('6be41a29-8fd3-407e-9b37-784a30492237', 'mugs-art1', '2023-03-27 13:00:58', '')
+INSERT INTO gift_shop_sales VALUES ('c7974efc-cb45-11ed-afa1-0242ac120002', 'mugs-art1', '2023-03-27 13:00:58', '6be41a29-8fd3-407e-9b37-784a30492237');
+
+INSERT INTO gift_shop_sales VALUES ('dc4f2f5e-cb45-11ed-afa1-0242ac120002', 'pai-ang-bla', '2023-03-27 14:08:52', 'c8304586-5661-4db7-8e75-4dd44b4f7648');
 -- CREATE TABLE gift_shop_item (
 --     gift_SKU TEXT PRIMARY KEY,
 --     gift_name TEXT NOT NULL,
