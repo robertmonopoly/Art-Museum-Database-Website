9a91ea32fd1d62155b379fe3ceb0a8e2246dd2cf
Michelle Nguyen <michellehnguyen03@gmail.com>
2023-04-12 02:20:33 -0500

    Fixed donation error, hopefully

--- src/donation.py
+++ src/donation.py
@@ -1,23 +1,24 @@
 # import
 import uuid
 from datetime import date
-
+# 3rd report
 def retrieve_donations_data(cur):
-    cur.execute("""SELECT d.donation_transaction_id, u.first_name, u.last_name, 
-    d.donator_email, SUM(d.donation_amount) as total_amount, u.membership, d.donation_on
+    # dont need uuid in report
+    cur.execute("""SELECT u.first_name, u.last_name, 
+    d.donator_email, d.donation_amount, u.membership, d.donation_on
     FROM donation as d
     INNER JOIN user_account as u
     ON d.donator_email = u.email
-    GROUP BY d.donation_transaction_id, u.first_name, u.last_name, d.donator_email, u.membership, d.donation_on
+    GROUP BY u.first_name, u.last_name, d.donator_email, d.donation_amount, u.membership, d.donation_on
     """)
     data = cur.fetchall()
     return data 
 
 def retrieve_donation_sum(cur):
-        cur.execute("""SELECT SUM(donation_amount)
-        FROM donation""")
-        data = cur.fetchone()
-        return data
+    cur.execute("""SELECT SUM(donation_amount)
+    FROM donation""")
+    data = cur.fetchone()
+    return data
 
 def insert_donation(cur, conn, email_address, money_amount):
     try:
